d824cea04e351c71da0aba6cb19ef18dda2efd90
Ahmed <ahmedali.96@hotmail.com>
2021-02-24 17:20:38 -0500

    shortened code and added comment

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const app = {};
 
 app.coinswanted = 0;
 
-app.getCryptoData = () => {
-  return $.ajax({
+app.getCryptoData = () =>
+  $.ajax({
     url: "https://api.coincap.io/v2/assets",
     method: "GET",
     dataType: "JSON",
@@ -11,71 +11,60 @@ app.getCryptoData = () => {
     const controlledAmount = app.coinswanted > 100 ? 100 : app.coinswanted;
     return res.data.slice(0, controlledAmount);
   });
-};
 
-app.formatPrice = (x) => {
-  let num = x.toFixed(2);
-  return `$${num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
-};
+app.formatPrice = (n) =>
+  `$${n
+    .toFixed(2)
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
 
-app.formatLargeNumbers = (n) => {
-  if (n >= 1e12) {
-    return +(n / 1e12).toFixed(1) + "T";
-  } else if (n >= 1e9 && n < 1e12) {
-    return +(n / 1e9).toFixed(1) + "B";
-  } else if (n >= 1e6 && n < 1e9) {
-    return +(n / 1e6).toFixed(1) + "M";
-  } else if (n >= 1e3 && n < 1e6) {
-    return +(n / 1e3).toFixed(1) + "K";
-  } else return n;
-};
+app.formatLargeNumbers = (n) =>
+  n >= 1e12
+    ? (n / 1e12).toFixed(2) + "T"
+    : n >= 1e9 && n < 1e12
+    ? (n / 1e9).toFixed(2) + "B"
+    : n >= 1e6 && n < 1e9
+    ? (n / 1e6).toFixed(2) + "M"
+    : n >= 1e3 && n < 1e6
+    ? (n / 1e3).toFixed(2) + "K"
+    : n;
 
-app.setCryptoData = () => {
-  const cryptoData = app.getCryptoData();
+app.setCryptoData = () =>
+  app.getCryptoData().done((res) =>
+    res.forEach((i) =>
+      $(".dataCont").append(`
+      <div class="cryptoItem">
 
-  cryptoData.done((res) => {
-    return res.forEach((i) => {
-      console.log(i);
-      const cryptoItem = `
-            <div class="cryptoItem">
+        <div class='itemHeader' >
+          <h2> #${i.rank} </h2>
+          <h2> ${i.symbol} </h2>
+        </div>
 
-            <div class='itemHeader' >
-                <h2> #${i.rank} </h2>
-                <h2> ${i.symbol} </h2>
-            </div>
+        <h1>${i.name}</h1>
+        <p> Price: ${app.formatPrice(Number(i.priceUsd))} </p>
+        <p> Market Cap: $${app.formatLargeNumbers(Number(i.marketCapUsd))} </p>
+        <p> 24 hour volume: $${app.formatLargeNumbers(
+          Number(i.volumeUsd24Hr)
+        )} </p>
+        <a href='${i.explorer}' target="_blank" > Explorer Link  </a>
 
-            <h1>${i.name}</h1>
-            <p> Price: ${app.formatPrice(Number(i.priceUsd))} </p>
-            <p> Market Cap: ${app.formatLargeNumbers(
-              Number(i.marketCapUsd)
-            )} </p>
-            <p> 24 hour volume: ${app.formatLargeNumbers(
-              Number(i.volumeUsd24Hr)
-            )} </p>
-            <a href='${i.explorer}' target=”_blank” > Explorer Link  </a>
+      </div>
+        `)
+    )
+  );
 
-            </div>
-              `;
-
-      $(".dataCont").append(cryptoItem);
-    });
-  });
-};
-
-app.addCoin = () => {
+app.addCoin = () =>
   $("#addCoin").click(() => {
     app.coinswanted++;
     $(".dataCont").empty();
     app.setCryptoData();
   });
-};
 
-app.clear = () => {
+app.clear = () =>
   $("#clear").click(() => {
     app.coinswanted = 0;
     $(".dataCont").empty();
   });
-};
 
 app.init = () => {
   app.addCoin();
@@ -83,6 +72,29 @@ app.init = () => {
   app.setCryptoData();
 };
 
-$(() => {
-  app.init();
-});
+$(() => app.init());
+
+// My goal was to finish my script page with the least amount of lines possible
+// app.formatLargeNumbers() isnt easy on the eyes, here's the samee code in an if statement
+
+// const formatLargeNumbers = () => {
+
+//   if (n >= 1e12) {
+
+//     return (n / 1e12).toFixed(2) + "T";
+
+//   } else if (n >= 1e9 && n < 1e12) {
+
+//     return (n / 1e9).toFixed(2) + "B";
+
+//   } else if (n >= 1e6 && n < 1e9) {
+
+//     return (n / 1e6).toFixed(2) + "M";
+
+//   } else if (n >= 1e3 && n < 1e6) {
+
+//     return (n / 1e3).toFixed(2) + "K";
+
+//   } else return n;
+
+// };
